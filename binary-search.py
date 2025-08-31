0bcef6994d1b8bad8ed7e30685fcc3764ccbf5d3
Talatam-prakash <talatamprakashnaidu@Talatams-Macbook-Air.local>
2025-08-31 14:16:34 +0530

    binary search functionality is added

---
+++ binary-search.py
@@ -0,0 +1,20 @@
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    
+    while left <= right:
+        mid = left + (right - left) // 2        
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    
+    return -1
+
+# Example usage:
+arr = [1, 2, 3, 4, 5]
+target = 5
+result = binary_search(arr, target)
+print(f"Element found at index: {result}" if result != -1 else "Element not found")
